d0f64ba7796a262133771c7180e132a0d30e57a9
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2022-11-27 23:17:38 +0800

    Update startup.py

--- startup.py
+++ startup.py
@@ -18,24 +18,10 @@ parser.add_argument('--rank', type=int, default=0, help='node rank')
 parser.add_argument('--world_size', type=int, default=8, help='world size')
 args, unparsed = parser.parse_known_args()
 
-master_host = os.environ['VC_WORKER_HOSTS'].split(',')[0]
-master_addr = master_host.split(':')[0]
-master_port = '8524'
-# FLAGS.worldsize will be re-computed follow as FLAGS.ngpu*FLAGS.nodes_num
-# FLAGS.rank will be re-computed in main_worker
-modelarts_rank = args.rank  # ModelArts receive FLAGS.rank means node_rank
-modelarts_world_size = args.world_size  # ModelArts receive FLAGS.worldsize means nodes_num
-os.environ['MASTER_ADDR'] = master_addr
-os.environ['MASTER_PORT'] = master_port
-
-print(f'IP: {master_addr},  Port: {master_port}')
-print(f'modelarts rank {modelarts_rank}, world_size {modelarts_world_size}')
-
 #######################################################################################################
 
 cmd_str = f"python -m torch.distributed.launch --nproc_per_node {args.num_gpus} \
-    --nnodes={modelarts_world_size} --node_rank={modelarts_rank} --master_addr={master_addr} \
-    --master_port={master_port} main_pretrain.py --batch_size {args.batch_size} --epochs {args.epochs} \
+    --nnodes={args.world_size} main_pretrain.py --batch_size {args.batch_size} --epochs {args.epochs} \
     --model {args.model} --warmup_epochs {args.warmup_epochs} --mask_ratio {args.mask_ratio} \
     --blr {args.blr}"
 
