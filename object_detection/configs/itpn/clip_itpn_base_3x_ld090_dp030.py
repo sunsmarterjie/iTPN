dd65e691f36240bdcb578664d567e8bde256e693
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2022-12-08 12:05:17 +0800

    Update clip_itpn_base_3x_ld090_dp030.py

--- object_detection/configs/itpn/clip_itpn_base_3x_ld090_dp030.py
+++ object_detection/configs/itpn/clip_itpn_base_3x_ld090_dp030.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/mask_rcnn_r50_fpn.py',
     '../_base_/datasets/coco_instance.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/schedule_3x.py', '../_base_/default_runtime.py'
 ]
 
 pretrained = '/cache/pretrain.pth'  # noqa
