ad6a2f4c1faa292fe7b9734c000bbb6538a27aee
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-02-07 20:38:30 +0800

    Update startup_ft.py

--- startup_ft.py
+++ startup_ft.py
@@ -32,26 +32,16 @@ parser.add_argument('--rank', type=int, default=0, help='node rank')
 parser.add_argument('--world_size', type=int, default=4, help='world size')
 parser.add_argument('--data_path', default='../imagenet/', type=str,
                     help='dataset path')
+parser.add_argument('--init_method', type=str, default='tcp://127.0.0.1:6666')
 args, unparsed = parser.parse_known_args()
 
-master_host = os.environ['VC_WORKER_HOSTS'].split(',')[0]
-master_addr = master_host.split(':')[0]
-master_port = '8524'
-# FLAGS.worldsize will be re-computed follow as FLAGS.ngpu*FLAGS.nodes_num
-# FLAGS.rank will be re-computed in main_worker
-modelarts_rank = args.rank  # ModelArts receive FLAGS.rank means node_rank
-modelarts_world_size = args.world_size  # ModelArts receive FLAGS.worldsize means nodes_num
+master_addr = args.init_method[:-5]
+master_port = args.init_method[-4:]
 os.environ['MASTER_ADDR'] = master_addr
 os.environ['MASTER_PORT'] = master_port
 
-print(f'IP: {master_addr},  Port: {master_port}')
-print(f'modelarts rank {modelarts_rank}, world_size {modelarts_world_size}')
-
-###################################################################################################
-
-
 cmd_str = f"python -m torch.distributed.launch --nproc_per_node {args.num_gpus} \
-    --nnodes={modelarts_world_size} --node_rank={modelarts_rank} --master_addr={master_addr} \
+    --nnodes={args.wprld_size} --node_rank={args.rank} --master_addr={master_addr} \
     --master_port={master_port} main_finetune.py --finetune {args.weight} --batch_size {args.batch_size} \
     --epochs {args.epochs} --model {args.model} --warmup_epochs {args.warmup_epochs} \
     --clip_grad {args.clip_grad} --blr {args.blr} --drop_path {args.drop_path}  --min_lr {args.min_lr} \
