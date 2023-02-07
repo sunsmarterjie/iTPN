7321fb5b1926cf96fc78b6b1ce30337a02d718f4
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-02-07 19:30:37 +0800

    Update masked_autoencoder.py

--- models/masked_autoencoder.py
+++ models/masked_autoencoder.py
@@ -1,12 +1,9 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
-
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
-# References:
-# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
-# DeiT: https://github.com/facebookresearch/deit
+# Reference:
+# MAE: https://github.com/facebookresearch/mae
 # --------------------------------------------------------
 import torch
 import torch.nn as nn
