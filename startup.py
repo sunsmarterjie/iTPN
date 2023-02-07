8b1bc6222e15c775a4e0619ca0adb4af1198a58c
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-02-07 20:51:58 +0800

    Update startup.py

--- startup.py
+++ startup.py
@@ -5,17 +5,20 @@ import logging
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--epochs', type=int, default=1600, help='total pretraing epochs')
-parser.add_argument('--warmup_epochs', type=int, default=40, help='warmup epochs')
-parser.add_argument('--model', type=str, default='itpn_base_dec512d8b', help='the default model')
-parser.add_argument('--data_path', type=str, default='../imagenet', help='the path of the dataset')
-parser.add_argument('--mask_ratio', default=0.75, type=float, help='ratio of the visual tokens/patches need be masked')
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--epochs', type=int, default=1600)
+parser.add_argument('--warmup_epochs', type=int, default=40)
+parser.add_argument('--model', type=str, default='itpn_base_dec512d8b', metavar='MODEL', 
+                    help='the name of model to train')
+parser.add_argument('--data_path', type=str, default='../imagenet',
+                    help='the path of the dataset')
+parser.add_argument('--mask_ratio', default=0.75, type=float, 
+                    help='ratio of the visual tokens/patches need be masked')
 parser.add_argument('--blr', type=float, default=1.5e-4, metavar='LR',
                     help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
 
 parser.add_argument('--num_gpus', type=int, default=8, help='the number of gpus')
-parser.add_argument('--rank', type=int, default=0, help='get your machine node rank')
+parser.add_argument('--rank', type=int, default=0, help='the machine rank')
 parser.add_argument('--world_size', type=int, default=8, help='how many machine you use')
 parser.add_argument('--init_method', type=str, default='tcp://127.0.0.1:6666')
 args, unparsed = parser.parse_known_args()
