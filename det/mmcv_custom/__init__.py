6aa2250350cf4309ae31b7ec8c4f3f1f631335b3
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-06-28 13:16:00 +0800

    Update __init__.py

--- det/mmcv_custom/__init__.py
+++ det/mmcv_custom/__init__.py
@@ -3,5 +3,6 @@
 from .checkpoint import load_checkpoint
 from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
 from .hivit_layer_decay_optimizer_constructor import HiViTLayerDecayOptimizerConstructor
+from ..backbone import *
 
 __all__ = ['load_checkpoint']
