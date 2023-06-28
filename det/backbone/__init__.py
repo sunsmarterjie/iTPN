5e62e37f209dd977063d37ed6b14415b32055665
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-06-29 00:06:36 +0800

    Delete det directory

--- det/backbone/__init__.py
+++
@@ -1,6 +0,0 @@
-from .iTPN_pixel import iTPN_pixel
-from .iTPN_CLIP import iTPN_CLIP
-
-__all__ = [
-    'iTPN_CLIP', 'iTPN_pixel'
-]
--- det/mmcv_custom/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .checkpoint import load_checkpoint
-from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
-from .hivit_layer_decay_optimizer_constructor import HiViTLayerDecayOptimizerConstructor
-from ..backbone import *
-
-__all__ = ['load_checkpoint']
