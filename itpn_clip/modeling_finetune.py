6fae341e451c1186888d3fff0d1c80b4a34d9a36
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2023-07-14 14:25:11 +0800

    Update modeling_finetune.py

--- itpn_clip/modeling_finetune.py
+++ itpn_clip/modeling_finetune.py
@@ -98,7 +98,7 @@ class BlockWithRPE(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-        if init_values is not None:
+        if init_values is not None and init_values > 0:
             if self.attn is not None:
                 self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)), requires_grad=True)
             else:
